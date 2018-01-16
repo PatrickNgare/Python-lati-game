620dc7f1e4cf72b611728417b20c71707775b5d6
patrickNgare <patrickngare@gmail.com>
2018-01-16 16:46:37 +0300

    added the code

---
+++ fizzBuzz.py
@@ -0,0 +1,10 @@
+number = range(1,100)
+for i in number:
+   if i % 3 == 0:
+       print ("Fizz")
+   elif i % 5 == 0:
+       print ("Buzz")
+   elif i % 5 == 0 and i % 3 == 0:
+       print ("FizzBuzz")
+   else:
+        print(i)
